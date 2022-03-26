10f4f84e6b432fd92d60f4a9a280d35e6318b6df
Daniel Busch <70059634+dakopen@users.noreply.github.com>
2022-03-26 23:27:38 +0100

    Dropdown fixed

--- webprojekt/static/assets/js/index.js
+++ webprojekt/static/assets/js/index.js
@@ -1,9 +1,5 @@
 const dropdown = $('.dropdown');
 
-
-
-
-
 //JQUERY
 /* PAGE LOAD */
 (function($) {
@@ -30,10 +26,57 @@ function makeid() {
     return text;
 }
 
-function generieren(){let generierobjekt=dropdown.find('input').val();let response=satzGeneratorBackend(generierobjekt);}
-function satzGeneratorBackend(data){parameterUrl="/satzgenerator/?session="+sessionid;console.log(parameterUrl)
-fetch(parameterUrl,{method:"post",body:data,headers:{"X-CSRFToken":getCookie('csrftoken'),"TEXTAREAVALUE":textarea.value.replace(/\s+/g,' ').trim(),},}).then(function(data){data.text().then(text=>{textarea.value=text;windowResize();resizeTextArea();});});}
-/* GENERATOR */
-function generieren() {
 
+
+/*dropdown.click(function(){$(this).attr('tabindex',1).focus();
+$(this).toggleClass('active');
+$(this).find('.dropdown-menu').slideToggle(300);});
+$('.dropdown').focusout(function(){$(this).removeClass('active');
+$(this).find('.dropdown-menu').slideUp(300);});
+
+$('.dropdown .dropdown-menu li').click(function(){$(this).parents('.dropdown').find('span').text($(this).text());
+$(this).parents('.dropdown').find('input').attr('value',$(this).attr('content'));});
+function clearTextarea(){textarea.value="";resizeTextArea();textarea.focus();weiterueben_verstecken();}
+
+*/
+//function generieren(){let generierobjekt=dropdown.find('input').val();let response=satzGeneratorBackend(generierobjekt);}
+//function satzGeneratorBackend(data){parameterUrl="/satzgenerator/?session="+sessionid;console.log(parameterUrl)
+//fetch(parameterUrl,{method:"post",body:data,headers:{"X-CSRFToken":getCookie('csrftoken'),"TEXTAREAVALUE":textarea.value.replace(/\s+/g,' ').trim(),},}).then(function(data){data.text().then(text=>{textarea.value=text;windowResize();resizeTextArea();});});}
+/* GENERATOR */
+
+
+
+
+function toggle_dropdown(){
+    dropdown.attr('tabindex',1).focus();
+    dropdown.toggleClass("active");
+    dropdown.find(".dropdown-menu").slideToggle(300);
+}
+  
+function dropdown_fold(){
+    console.log("fold");
+    dropdown.removeClass("active");
+    dropdown.find(".dropdown-menu").slideUp(300);
+
+}
+
+/* Funktion: Dropdown-Item pressed */
+$('.dropdown .dropdown-menu li').click(function(){
+    dropdown.find("span").text($(this).attr("content"));
+    dropdown.find("input").attr("value", $(this).attr("content"))
+
+});
+
+
+
+
+/*
+$(this).parents('.dropdown').find('input').attr('value',$(this).attr('content'));});
+
+*/
+
+
+function generieren() {
+    let selected_category = dropdown.find("input").val();
+    console.log(selected_category);
 }
