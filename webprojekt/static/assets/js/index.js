f7b2c221d9d57dfd9ca67d7276a1016ded8d0118
Daniel Busch <70059634+dakopen@users.noreply.github.com>
2022-03-26 22:01:28 +0100

    Small changes

--- webprojekt/static/assets/js/index.js
+++ webprojekt/static/assets/js/index.js
@@ -1,3 +1,8 @@
+const dropdown = $('.dropdown');
+
+
+
+
 
 //JQUERY
 /* PAGE LOAD */
@@ -7,7 +12,8 @@
     // Page loading animation
     $(window).on('load', function() {
         $('#js-preloader').addClass('loaded');
-        sessionid = makeid();
+        var sessionid = makeid();
+        console.log("Session-ID: " + sessionid);
     });
 
 })(window.jQuery);
@@ -19,7 +25,15 @@ function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < 6; i++)
+    for (var i = 0; i < 8; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
     return text;
+}
+
+function generieren(){let generierobjekt=dropdown.find('input').val();let response=satzGeneratorBackend(generierobjekt);}
+function satzGeneratorBackend(data){parameterUrl="/satzgenerator/?session="+sessionid;console.log(parameterUrl)
+fetch(parameterUrl,{method:"post",body:data,headers:{"X-CSRFToken":getCookie('csrftoken'),"TEXTAREAVALUE":textarea.value.replace(/\s+/g,' ').trim(),},}).then(function(data){data.text().then(text=>{textarea.value=text;windowResize();resizeTextArea();});});}
+/* GENERATOR */
+function generieren() {
+
 }
